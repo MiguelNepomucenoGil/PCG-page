ea436c9f176a51da210559739f3a823eae7ab053
MiguelNepomucenoGil <85374522+MiguelNepomucenoGil@users.noreply.github.com>
2024-10-29 22:31:47 -0300

    Delete pagina PCG directory

--- pagina PCG/script.js
+++
@@ -1,6 +0,0 @@
-const toggler = document.querySelector('.navbar-toggler');
-const navbarNav = document.querySelector('.navbar-nav');
-
-toggler.addEventListener('click', () => {
-    navbarNav.classList.toggle('show'); // Alterna a classe 'show' no menu
-});
